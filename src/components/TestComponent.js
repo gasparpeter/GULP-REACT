0b3ba74da82d1445ef2369e2fb1caa01f032e20d
gasparpeter <petihappygaspar@gmail.com>
2019-09-03 06:26:44 -0700

    - babel-plugin-transform-es2015-modules-amd

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+import React, {PureComponent} from 'react';
+
+class TestComponent extends PureComponent {
+    render() {
+        return(
+            <div>Test Component:)</div>
+        )
+    }
+}
+
+export default TestComponent;
